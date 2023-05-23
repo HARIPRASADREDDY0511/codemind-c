b598e77d2eca64919aac11d7275ef071b0942612
HARIPRASADREDDY0511 <119730227+HARIPRASADREDDY0511@users.noreply.github.com>
2023-05-23 19:24:15 +0530

    #include<stdio.h> int main() {     int n,max=0,c=0;     scanf("%d",&n);     int i=0;     int a[100];     while(n!=0)     {         int r=n%10;         a[i]=r;         i++;         c++;         n=n/10;     }     for(int i=0;i<c;i++)     {         if(a[i]>max)         {             max=a[i];         }     }     printf("%d",max); }

---
+++ Largest_Digit.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int n,max=0,c=0;
+    scanf("%d",&n);
+    int i=0;
+    int a[100];
+    while(n!=0)
+    {
+        int r=n%10;
+        a[i]=r;
+        i++;
+        c++;
+        n=n/10;
+    }
+    for(int i=0;i<c;i++)
+    {
+        if(a[i]>max)
+        {
+            max=a[i];
+        }
+    }
+    printf("%d",max);
+}
